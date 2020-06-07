a8d387b9324637751c2d80e5ecc559efb6ee6fb9
kelpin hartanto <kelvinhartanto18@gmail.com>
2020-06-07 20:20:57 +0700

    fix it

--- resources/views/backend/laporan_spt/index.blade.php
+++ resources/views/backend/laporan_spt/index.blade.php
@@ -74,11 +74,6 @@
                                             class="btn btn-info" title="Detail"><i class="fa fa-eye"></i></a>
                                         <a href="{{ route('laporanspt.edit',['id' => $item->id_spt]) }}"
                                             class="btn btn-warning" title="Ubah"><i class="fa fa-edit"></i></a>
-                                        <button type="button" class="btn btn-danger" title="Hapus" data-toggle="modal"
-                                            data-target="#modal-default">
-                                            <i class="fa fa-trash"></i>
-                                        </button>
-
                                     </td>
                                 </tr>
                                 @endforeach
@@ -104,25 +99,6 @@
         </div>
     </div>
 </section>
-<div class="modal fade" id="modal-default">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header bg-kpp">
-                <h4 class="modal-title">Perhatian ! </h4>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <p>Apakah anda yakin ingin menghapus ?</p>
-            </div>
-            <div class="modal-footer justify-content-between">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Tidak</button>
-                <button type="button" class="btn btn-danger">Ya, Hapus</button>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
 @section('js')
 <!-- DataTables -->
