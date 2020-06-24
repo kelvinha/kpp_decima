2f976cbf6e6d9cc0b450e5a524e8fde6f24e86f5
kelpin hartanto <kelvinhartanto18@gmail.com>
2020-06-24 16:42:52 +0700

    db baru

--- database/kpp_decima.sql
+++ database/kpp_decima.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 08, 2020 at 07:57 AM
+-- Generation Time: Jun 24, 2020 at 04:41 PM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -93,7 +93,19 @@ CREATE TABLE `spt` (
 INSERT INTO `spt` (`id_spt`, `id_wp`, `npwp_wp`, `status_lapor`, `tanggal_lapor`, `created_at`, `updated_at`) VALUES
 (1, 1, '121210291201921', 'Sudah Lapor', '07 June 2020', '2020-05-29 08:12:17', '2020-06-07 06:29:52'),
 (2, 2, '131313131312121', 'Sudah Lapor', '07 June 2020', '2020-05-29 08:26:16', '2020-06-07 06:28:07'),
-(4, 5, '222222222222222', 'Belum Lapor', NULL, '2020-05-30 08:58:31', '2020-05-30 08:58:31');
+(4, 5, '222222222222222', 'Sudah Lapor', '24 June 2020', '2020-05-30 08:58:31', '2020-06-23 23:38:15'),
+(10, 11, '115443594763313', 'Belum Lapor', NULL, '2020-06-22 03:41:22', '2020-06-22 03:41:22'),
+(11, 12, '127217319078688', 'Belum Lapor', NULL, '2020-06-22 04:18:11', '2020-06-22 04:18:11'),
+(12, 13, '126749227784511', 'Belum Lapor', NULL, '2020-06-22 04:18:11', '2020-06-22 04:18:11'),
+(16, 17, '117510898314452', 'Sudah Lapor', '22 June 2020', '2020-06-22 04:22:30', '2020-06-22 04:43:07'),
+(17, 18, '129646198797463', 'Belum Lapor', NULL, '2020-06-22 04:22:30', '2020-06-22 04:22:30'),
+(18, 19, '130589478532067', 'Sudah Lapor', '22 June 2020', '2020-06-22 04:22:30', '2020-06-22 04:46:04'),
+(19, 20, '130948281665758', 'Belum Lapor', NULL, '2020-06-22 04:22:30', '2020-06-22 04:22:30'),
+(20, 21, '135016090749200', 'Belum Lapor', NULL, '2020-06-22 04:22:30', '2020-06-22 04:22:30'),
+(22, 23, '110812237347641', 'Belum Lapor', NULL, '2020-06-22 04:22:30', '2020-06-22 04:22:30'),
+(23, 24, '100784962856218', 'Belum Lapor', NULL, '2020-06-22 04:22:30', '2020-06-22 04:22:30'),
+(26, 27, '110344366073805', 'Belum Lapor', NULL, '2020-06-22 04:22:30', '2020-06-22 04:22:30'),
+(27, 28, '119245306593091', 'Belum Lapor', NULL, '2020-06-22 04:22:30', '2020-06-22 04:22:30');
 
 -- --------------------------------------------------------
 
@@ -131,12 +143,11 @@ CREATE TABLE `wajib_pajak` (
   `id_wp` int(11) NOT NULL,
   `npwp` varchar(16) DEFAULT NULL,
   `nama` varchar(20) NOT NULL,
-  `email` varchar(20) NOT NULL,
-  `no_hp` varchar(15) NOT NULL,
   `alamat` text NOT NULL,
   `kategori_wp` varchar(40) NOT NULL,
   `jenis_spt` varchar(25) NOT NULL,
   `tahun_pajak` varchar(11) NOT NULL,
+  `nama_seksi` varchar(20) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -145,10 +156,22 @@ CREATE TABLE `wajib_pajak` (
 -- Dumping data for table `wajib_pajak`
 --
 
-INSERT INTO `wajib_pajak` (`id_wp`, `npwp`, `nama`, `email`, `no_hp`, `alamat`, `kategori_wp`, `jenis_spt`, `tahun_pajak`, `created_at`, `updated_at`) VALUES
-(1, '121210291201921', 'Kris', 'suh@mail.com', '089898989898', 'Jakarta Selatan', 'Orang Pribadi (Induk)', 'SPT Masa', '2018', '2020-05-29 08:12:17', '2020-06-06 06:56:04'),
-(2, '131313131312121', 'Budi', 'badang@mail.com', '089898989898', 'Depok', 'Memilih Terpisah (MT)', 'SPT Tahunan', '2019', '2020-05-29 08:26:15', '2020-06-07 06:31:48'),
-(5, '222222222222222', 'Susi Susanti', 'rose@mail.com', '089999999999', 'Jakarta Selatan', 'Kantor Perwakilan Perusahaan Asing', 'SPT Masa', '2020', '2020-05-30 08:58:31', '2020-06-06 06:56:41');
+INSERT INTO `wajib_pajak` (`id_wp`, `npwp`, `nama`, `alamat`, `kategori_wp`, `jenis_spt`, `tahun_pajak`, `nama_seksi`, `created_at`, `updated_at`) VALUES
+(1, '121210291201921', 'Kris', 'Jakarta Selatan', 'Orang Pribadi (Karyawan)', '1770', '2018', 'Waskon 2', '2020-05-29 08:12:17', '2020-06-22 09:53:09'),
+(2, '131313131312121', 'Budi', 'Depok', 'Orang Pribadi (Non-Karyawan)', '1771', '2019', 'Waskon 3', '2020-05-29 08:26:15', '2020-06-22 09:53:19'),
+(5, '222222222222222', 'Susi Susanti', 'Jakarta Selatan', 'Badan', '1770', '2020', 'Waskon 1', '2020-05-30 08:58:31', '2020-06-22 09:53:49'),
+(11, '115443594763313', 'User 61', 'QsFPd1GfRb2bTrc', 'Badan', '1779', '2015', 'waskon 4', '2020-06-22 03:41:22', '2020-06-22 03:41:22'),
+(12, '127217319078688', 'User 40', 'uYV3m7lizGZFTPAkcPKz', 'Orang Pribadi (Karyawan)', '1774', '2019', 'waskon 2', '2020-06-22 04:18:11', '2020-06-22 04:18:11'),
+(13, '126749227784511', 'User 35', 'PSKZhjz5iVfPLnRHSJEi', 'Badan', '1774', '2017', 'waskon 3', '2020-06-22 04:18:11', '2020-06-22 04:18:11'),
+(17, '117510898314452', 'User 47', 'DLIm6Od32qB1HyjPbGIq', 'Badan', '1770', '2021', 'waskon 4', '2020-06-22 04:22:30', '2020-06-22 04:22:30'),
+(18, '129646198797463', 'User 22', '8mTVmyO8Y1kgSqZXiGps', 'Orang Pribadi (Karyawan)', '1779', '2014', 'waskon 3', '2020-06-22 04:22:30', '2020-06-22 04:22:30'),
+(19, '130589478532067', 'User 72', 'biYa661ndSCyxZH06LUO', 'Orang Pribadi (Non-Karyawan)', '1773', '2020', 'waskon 3', '2020-06-22 04:22:30', '2020-06-22 04:22:30'),
+(20, '130948281665758', 'User 89', 'BmKvW1kFCXnOlKbPCIRt', 'Orang Pribadi (Karyawan)', '1778', '2015', 'waskon 3', '2020-06-22 04:22:30', '2020-06-22 04:22:30'),
+(21, '135016090749200', 'User 25', '4Pwt0ViuUkDU5UAwmM78', 'Badan', '1779', '2021', 'waskon 3', '2020-06-22 04:22:30', '2020-06-22 04:22:30'),
+(23, '110812237347641', 'User 40', 'NGFFos5ilTUb5DuNlH1f', 'Badan', '1776', '2020', 'waskon 3', '2020-06-22 04:22:30', '2020-06-22 04:22:30'),
+(24, '100784962856218', 'User 89', 'sQOuo2uAGG9g7V9NJFBJ', 'Orang Pribadi (Karyawan)', '1778', '2014', 'waskon 3', '2020-06-22 04:22:30', '2020-06-22 04:22:30'),
+(27, '110344366073805', 'User 31', 'oN4b27LVxQCMcvyKtth2', 'Badan', '1774', '2021', 'waskon 3', '2020-06-22 04:22:30', '2020-06-22 04:22:30'),
+(28, '119245306593091', 'User 52', 'rzEZLUQSCXp9upMi1MU5', 'Orang Pribadi (Non-Karyawan)', '1770', '2021', 'waskon 2', '2020-06-22 04:22:30', '2020-06-22 04:22:30');
 
 --
 -- Indexes for dumped tables
@@ -211,7 +234,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `spt`
 --
 ALTER TABLE `spt`
-  MODIFY `id_spt` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_spt` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -223,7 +246,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `wajib_pajak`
 --
 ALTER TABLE `wajib_pajak`
-  MODIFY `id_wp` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_wp` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
